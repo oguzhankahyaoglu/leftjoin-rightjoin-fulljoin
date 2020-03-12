546430bb610a3d693a00c0292c7826d7c31d604a
oguzhan.kahyaoglu <oguzhan.kahyaoglu@enerjisauretim.com>
2020-03-12 10:12:27 +0300

    v2.2

--- JoinExtensions/JoinItem.cs
+++ JoinExtensions/JoinItem.cs
@@ -4,5 +4,10 @@
     {
         public TLeft Left { get; set; }
         public TRight Right { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Left}<--->{Right}";
+        }
     }
 }
